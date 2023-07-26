4dec6eac4dabdb184ae4a124de25ab825763d205
kaanpulular@gmail.com <kaanpulular@gmail.com>
2023-07-26 18:21:08 +0300

    Add project files.

---
+++ Command/CreatePdfTableActionCommand.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Command.Commands;
+
+namespace WebApp.Command.Command
+{
+    public class CreatePdfTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly PdfFile<T> _pdfFile;
+
+        public CreatePdfTableActionCommand(PdfFile<T> pdfFile)
+        {
+            _pdfFile = pdfFile;
+        }
+
+        public IActionResult Execute()
+        {
+            var excelMemoryStream = _pdfFile.Create();
+            return new FileContentResult(excelMemoryStream.ToArray(), _pdfFile.FileType)
+            { FileDownloadName = _pdfFile.FileName };
+        }
+    }
+}
---
+++ Command/FileCreateInvoker.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Command
+{
+    public class FileCreateInvoker
+    {
+        private  ITableActionCommand _tableActionCommand;
+        private List<ITableActionCommand> _tableActionCommands = new List<ITableActionCommand>();
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommands.Add(tableActionCommand);
+        }
+
+        public IActionResult CreateFile()
+        {
+            return _tableActionCommand.Execute();   
+        }
+
+        public List<IActionResult> CreateFiles()
+        {
+           
+           return _tableActionCommands.Select(x => x.Execute()).ToList();
+        }
+    }
+}
